729cd6cce4372c6cf93128bdc1b2809e361fee73
Shashwat Murawala <murawala.shashwat@gmail.com>
2026-09-23 11:54:45 -0300

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "h"
+print(type(variable))
+variable = []
+print(type(variable))
